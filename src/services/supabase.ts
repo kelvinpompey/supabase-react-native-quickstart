bdc71445f8131aa3957ae36e8f7c7403baa25a53
Kelvin Pompey <kelvin.pompey@gmail.com>
2021-11-09 07:20:13 -0500

    Initial commit

---
+++ src/services/supabase.ts
@@ -0,0 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createClient} from '@supabase/supabase-js';
+import {SUPABASE_ANON_KEY, SUPABASE_URL} from '../config';
+
+let client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  localStorage: AsyncStorage,
+  detectSessionInUrl: false,
+});
+
+(global as any).supabase = client;
+export default client;
